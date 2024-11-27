5e370975920ebc734079437be25a8ee107a6dea0
cx-keval <keval.culturex@gmail.com>
2024-11-27 17:16:26 +0530

    CI: removing unwanted files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,3 @@
-import PageEight from "./PageEight";
-import PageFive from "./PageFive";
-import PageFour from "./PageFour";
-import PageOne from "./PageOne";
-import PageSeven from "./PageSeven";
-import PageSix from "./PageSix";
-import PageThree from "./PageThree";
-import PageTwo from "./PageTwo";
 import ProfileOverviewWithSocialScore from "./ProfileOverviewWithSocialScore";
 import ContentAnalysis from "./ContentAnalysis";
 import AudienceOverviewCredibilityGeography from "./AudienceOverviewCredibilityGeography";
@@ -17,14 +9,6 @@ import TopHashtagsAndMentions from "./TopHashtagsAndMentions";
 import PostEngagementPage from "./PostEngagementPage";
 
 export {
-    PageOne,
-    PageTwo,
-    PageThree,
-    PageFour,
-    PageFive,
-    PageSix,
-    PageSeven,
-    PageEight,
     ProfileOverviewWithSocialScore,
     ContentAnalysis,
     AudienceOverviewCredibilityGeography,
